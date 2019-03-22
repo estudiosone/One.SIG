0d35b208b764a8d4b1ecc77cd7336441b7756b83
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2019-03-21 21:49:43 -0300

    Modelo

---
+++ One.SIG.Sistema.Datos.Modelo/Direccion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace One.SIG.Sistema.Datos.Modelo
+{
+    public class Direccion
+    {
+        public string Pais { get; set; }
+        public string Estado { get; set; }
+        public string Ciudad { get; set; }
+        public string Barrio { get; set; }
+        public string Calle { get; set; }
+        public string Puerta { get; set; }
+        public int CodigoPostal { get; set; }
+    }
+}
---
+++ One.SIG.Sistema.Datos.Modelo/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace One.SIG.Sistema.Datos.Modelo
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string NombreSegundo { get; set; }
+        public string Apellido { get; set; }
+        public string ApellidoSegundo { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public int Edad
+        {
+            get
+            {
+                var a = DateTime.Now - FechaNacimiento;
+                return Convert.ToInt32(a.Days / 365);
+            }
+        }
+        public string TipoDeDocumento { get; set; }
+        public string Documento { get; set; }
+    }
+}
