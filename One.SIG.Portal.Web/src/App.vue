d22d31b5d1548c4c444e22c974262f35f23c8888
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2019-03-24 21:34:09 -0300

    Web

--- One.SIG.Portal.Web/src/App.vue
+++ One.SIG.Portal.Web/src/App.vue
@@ -1,7 +1,8 @@
-<template lang="pug">
-  div#id
-    img(alt="Vue logo" src="./assets/logo.png")
-    HelloWorld(msg="Welcome to Your Vue.js App")
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
