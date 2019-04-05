e6b90fcfa13c181f5220921916df905a736a4928
Diego Rodriguez <drodriguez@eldescubrimiento.local>
2019-04-04 21:49:03 -0300

    varios

--- One.SIG.Consola/Program.cs
+++ One.SIG.Consola/Program.cs
@@ -16,38 +16,85 @@ namespace One.SIG.Consola
             var awsAccessKeyId = "AKIAIN7WGGIWGI5LLMJA";
             var awsSecretAccessKey = "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";
 
-            var body =
-                "<div>" +
-                "   Buenos días, Diego" +
-                "   <br>" +
-                "   ¡El motivo de mi contacto es ofrecerle una promoción imperdible!" +
-                "   <br>" +
-                "   <img style=\"max - width:100 %; display: inline - block\" src=\"http://www.livebeep.com/visitor/medias/file/image/large/163/p/Promo-Turismo-feed-jpg-210226.jpg\">" +
-                "</div>";
+            //var body =
+            //    "<div>" +
+            //    "   Buenos días, Diego" +
+            //    "   <br>" +
+            //    "   ¡El motivo de mi contacto es ofrecerle una promoción imperdible!" +
+            //    "   <br>" +
+            //    "   <img style=\"max - width:100 %; display: inline - block\" src=\"http://www.livebeep.com/visitor/medias/file/image/large/163/p/Promo-Turismo-feed-jpg-210226.jpg\">" +
+            //    "</div>";
             var client = new Amazon.SimpleEmail.AmazonSimpleEmailServiceClient(awsAccessKeyId, awsSecretAccessKey, Amazon.RegionEndpoint.USEast1);
 
-            var result = client.SendEmail(new Amazon.SimpleEmail.Model.SendEmailRequest()
-            {
-                Source = "El Descubrimiento RESORT CLUB <noreplay@eldescubrimiento.com>",
-                ReplyToAddresses = new List<string> { "El Descubrimiento RESORT CLUB <info@eldescubrimiento.com>" },
-                Destination = new Amazon.SimpleEmail.Model.Destination()
+            var createTemplateResponse = client.CreateTemplate(
+                new Amazon.SimpleEmail.Model.CreateTemplateRequest()
                 {
-                    ToAddresses = new List<string>()
+                    Template = new Amazon.SimpleEmail.Model.Template()
                     {
-                          "tecnica@eldescubrimiento.com"
+                        TemplateName = "T001",
+                        SubjectPart = "Promoción de prueba",
+                        HtmlPart =
+                        "<div>" +
+                        "   Buenos días, {{name}}!" +
+                        "   <br>" +
+                        "   ¡El motivo de mi contacto es ofrecerle una promoción imperdible!" +
+                        "   <br>" +
+                        "   <img style=\"max - width:100 %; display: inline - block\" src=\"http://www.livebeep.com/visitor/medias/file/image/large/163/p/Promo-Turismo-feed-jpg-210226.jpg\">" +
+                        "</div>"
+                    }
+                });
+
+
+            var sendBulkTemplatedEmailResponse = client.SendBulkTemplatedEmail(new Amazon.SimpleEmail.Model.SendBulkTemplatedEmailRequest()
+            {
+                ConfigurationSetName = "Marketing",
+                Destinations = new List<Amazon.SimpleEmail.Model.BulkEmailDestination>()
+                {
+                    new Amazon.SimpleEmail.Model.BulkEmailDestination()
+                    {
+                         Destination = new Amazon.SimpleEmail.Model.Destination(new List<string>(){ "tecnica@eldescubrimiento.com" }),
+                         ReplacementTemplateData = "{\"name\":\"Diego\"}"
+                    },
+                    new Amazon.SimpleEmail.Model.BulkEmailDestination()
+                    {
+                         Destination = new Amazon.SimpleEmail.Model.Destination(new List<string>(){ "diegorrod91@gmail.com" }),
+                         ReplacementTemplateData = "{\"name\":\"Diego Rodríguez\"}"
                     }
                 },
-                Message = new Amazon.SimpleEmail.Model.Message()
-                {
-                    Subject = new Amazon.SimpleEmail.Model.Content("Prueba"),
-                    Body = new Amazon.SimpleEmail.Model.Body()
-                    {
-                        Html = new Amazon.SimpleEmail.Model.Content(body)
-                    }
-                }
+                Source = "El Descubrimiento RESORT CLUB <noreplay@eldescubrimiento.com>",
+                ReplyToAddresses = new List<string> { "El Descubrimiento RESORT CLUB <info@eldescubrimiento.com>" },
+                Template = "T001",
+                DefaultTemplateData = "{\"name\":\"estimado cliente\"}"
             });
-            Console.WriteLine(result);
-            Console.ReadKey();
+
+            var deleteTemplateResponse = client.DeleteTemplate(new Amazon.SimpleEmail.Model.DeleteTemplateRequest()
+            {
+                TemplateName = "T001",
+            });
+
+            //var result = client.SendEmail(new Amazon.SimpleEmail.Model.SendEmailRequest()
+            //{
+            //    Source = "El Descubrimiento RESORT CLUB <noreplay@eldescubrimiento.com>",
+            //    ReplyToAddresses = new List<string> { "El Descubrimiento RESORT CLUB <info@eldescubrimiento.com>" },
+            //    Destination = new Amazon.SimpleEmail.Model.Destination()
+            //    {
+            //        ToAddresses = new List<string>()
+            //        {
+            //              "tecnica@eldescubrimiento.com"
+            //        }
+            //    },
+            //    Message = new Amazon.SimpleEmail.Model.Message()
+            //    {
+            //        Subject = new Amazon.SimpleEmail.Model.Content("Prueba"),
+            //        Body = new Amazon.SimpleEmail.Model.Body()
+            //        {
+            //            Html = new Amazon.SimpleEmail.Model.Content(body)
+            //        }
+            //    },
+            //    ConfigurationSetName = "Marketing",
+            //});
+            //Console.WriteLine(result.MessageId);
+            //Console.ReadKey();
         }
     }
 }
